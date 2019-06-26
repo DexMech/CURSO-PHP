979cf9a13362c6d1778309ab7fd0e2ea49491f9f
alex <alex.junior.carlos23@gmail.com>
2019-06-26 19:10:31 -0300

    second

---
+++ exercicio.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+<link rel="stylesheet" href="assets/css/exercicio.css">
+</head>
+
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualizacao de Exercicio</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">sem formatacao</a>
+        <a href="#" class="vermelho">voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS <?=  date('Y')?>
+    </footer>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Indice de Exercicios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo">
+                    <h3>Modulo 01</h3>
+                    <ul>
+                        <li><a href="exercicio.php">Exercicio 1</a></li>
+                        <li><a href="#">Exercicio 2</a></li>
+                        <li><a href="#">Exercicio 3</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Modulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercicio 1</a></li>
+                        <li><a href="#">Exercicio 2</a></li>
+                        <li><a href="#">Exercicio 3</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Modulo 03</h3>
+                    <ul>
+                        <li><a href="#">Exercicio 1</a></li>
+                        <li><a href="#">Exercicio 2</a></li>
+                        <li><a href="#">Exercicio 3</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Modulo 04</h3>
+                    <ul>
+                        <li><a href="#">Exercicio 1</a></li>
+                        <li><a href="#">Exercicio 2</a></li>
+                        <li><a href="#">Exercicio 3</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Modulo 05</h3>
+                    <ul>
+                        <li><a href="#">Exercicio 1</a></li>
+                        <li><a href="#">Exercicio 2</a></li>
+                        <li><a href="#">Exercicio 3</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Modulo 06</h3>
+                    <ul>
+                        <li><a href="#">Exercicio 1</a></li>
+                        <li><a href="#">Exercicio 2</a></li>
+                        <li><a href="#">Exercicio 3</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Modulo 07</h3>
+                    <ul>
+                        <li><a href="#">Exercicio 1</a></li>
+                        <li><a href="#">Exercicio 2</a></li>
+                        <li><a href="#">Exercicio 3</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Modulo 08</h3>
+                    <ul>
+                        <li><a href="#">Exercicio 1</a></li>
+                        <li><a href="#">Exercicio 2</a></li>
+                        <li><a href="#">Exercicio 3</a></li>
+                    </ul>
+                </div>
+                <div class="modulo">
+                    <h3>Modulo 09</h3>
+                    <ul>
+                        <li><a href="#">Exercicio 1</a></li>
+                        <li><a href="#">Exercicio 2</a></li>
+                        <li><a href="#">Exercicio 3</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS <?=  date('Y')?>
+    </footer>
+
+</body>
+
+</html>
